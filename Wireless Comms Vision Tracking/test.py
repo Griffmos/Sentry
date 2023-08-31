6b91a65ec923bf83d1d54084cbdfdcf1ee645e58
Griffmos <griffhero8@icloud.com>
2023-08-30 22:54:31 -0400

    wireless tracking works!

--- Wireless Comms Vision Tracking/test.py
+++ Wireless Comms Vision Tracking/test.py
@@ -4,11 +4,14 @@ from time import sleep
 
 def main():
 
-    tracker = clientPersonTracking.tracker(True, '', '8888')
+    tracker = clientPersonTracking.tracker(True, '192.168.1.43', 8888)
 
 
     while True:
-        tracker.findTarget()
+        success=tracker.findTarget()
+        
         print(tracker.currTarget)
 
-        sleep(0.1)
+
+
+main()

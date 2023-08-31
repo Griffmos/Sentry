3c9faeffd500ccd5f453c6630b1512ceb3f45bab
Griffmos <griffhero8@icloud.com>
2023-08-30 20:25:09 -0400

    took this from the spsand

---
+++ Wireless Communication Testing/serverTestArrays2.py
@@ -0,0 +1,154 @@
+#code from spsand branch
+
+import socket
+import numpy
+import cv2
+
+def recvIntArr(s:socket.socket,maxByteSize:int=4,maxDimensions=3):
+    data:bytes=[]
+    amtBytes:int=-1
+    arrDimensions=[]
+    numDimensions:int
+    bytesPerInt:-1
+
+    arr=None
+
+
+
+    #getting base info
+    # while amtBytes ==-1:
+    #     if (data is None):
+    #         data=s.recv(50)
+    #     else:
+    #         data+=s.recv(50)
+        
+
+    #     #parse order where (x) is x bytes: amtBytes(6), bytesPerInt(1), dimensions(1),shape(3)
+
+    #     #maxSize bytes for amtBytes + 1 byte for bytesPerInt + 1 byte for dimensions + maxDimesions*2 bytes for a 16 bit max size in each direction 
+    #     infoBytesParsed:int=0
+    #     # if (len(data)>=(maxByteSize-1)+1+1+maxDimensions*2):
+    #     #     amtBytes=int.from_bytes(data[0:maxByteSize], "big")
+    #     #     print(f'amtBytes: {amtBytes}')
+    #     #     infoBytesParsed+=maxByteSize
+
+    #     #     bytesPerInt=data[maxByteSize]
+    #     #     print(f'bytesPerInt: {bytesPerInt}')
+    #     #     infoBytesParsed+=1
+
+    #     #     numDimensions=int(data[maxByteSize+1])
+    #     #     print(f'numDimensions: {numDimensions}')
+    #     #     infoBytesParsed+=1
+
+    #     #     for i in range(0,numDimensions*2, 2):
+    #     #         #maxSize+2 to account for the data parsing above, i+2 to accound for each appenened int is a 2 byte int
+    #     #         arrDimensions.append(int.from_bytes(data[maxByteSize+2+i:maxByteSize+2+i+2],"big"))
+    #     #         infoBytesParsed+=2
+
+    #     #     print(f'arrDimensions: {arrDimensions}')
+
+    #     #     data=data[infoBytesParsed:len(data)]
+    #     print(infoBytesParsed)
+            
+    bP:int=0 #bytesParsed
+
+    #incrementors=numpy.zeros(shape=numDimensions,dtype=numpy.int16)
+
+
+    #limited to hold either 8 bit or 16 bit ints, and 3d array
+    #arr=numpy.empty(shape=arrDimensions,dtype=(numpy.uint8 if (bytesPerInt==1) else numpy.uint16))
+
+    while len(data) < 921600:
+        data+=s.recv(921600)
+        #print(len(data))
+
+        #parsing each data
+
+    #     thisRecvBP=0
+
+    #     #print(data)
+        
+    #     while(thisRecvBP<len(data)):
+
+
+    #         bPBefore=thisRecvBP
+
+    #         #getting byte to write
+
+    #         byte=None
+
+    #         if (bytesPerInt==1):
+    #             byte=data[thisRecvBP]
+    #         else:
+    #             byte=int.from_bytes(data[thisRecvBP:thisRecvBP+2],"big")
+
+    #         #print(byte)
+            
+    #         #getting to the right dimension
+    #         if len(arrDimensions)==1:
+    #             arr[incrementors[0]]=byte
+    #         elif len(arrDimensions)==2:
+    #             arr[incrementors[0]][incrementors[1]]=byte
+    #         elif len(arrDimensions)==3:     
+    #             arr[incrementors[0]][incrementors[1]][incrementors[2]]=byte
+            
+            
+    #         thisRecvBP+=bytesPerInt
+
+    #         #incrementing whatever the least significant incrementer is
+    #         incrementors[len(incrementors)-1]+=1
+
+
+    #         for i in range(len(incrementors)):
+    #             #if the incrementor is = the dimension, it goes to zero and the next most significant increases
+                
+    #             if (incrementors[len(incrementors)-1-i]==arrDimensions[len(incrementors)-1-i]):
+    #                 incrementors[len(incrementors)-1-i]=0
+    #                 incrementors[len(incrementors)-1-i-1]+=1 #this MIGHT error out if this byte is the last possible value and it tries to increment the next most significant incrementor which doesn't exists
+    #             else: #if this one didn't increment, then next one def won't
+    #                 break
+
+
+    #         #indicates couldn't parse anything since there is a hanging byte for the next data recieve
+    #         if (bPBefore==thisRecvBP):
+    #             data=data[thisRecvBP:amtBytes] #should leave remainder
+    #             print("help!")
+    #             break
+                
+    #     print(thisRecvBP)
+    #     bP+=thisRecvBP
+
+    # print(bP)
+    arr = numpy.asarray(data,numpy.uint8)
+    arr = numpy.reshape(arr, [480,640,3])
+
+    return arr
+
+
+
+
+
+
+
+
+def main():
+    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('',8888))
+    s.listen(1)
+    print("server runnig")
+    print(s.getsockname())
+
+    conn,address = s.accept()
+    print(f"Connection from {address} worked!")
+
+    
+    count=0
+
+    while True:
+        arr=recvIntArr(conn)
+        cv2.imshow('frame',arr)
+        count+=1
+
+
+
+main()
